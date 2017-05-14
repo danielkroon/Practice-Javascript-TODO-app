fedc027a9ed88a56db1fdf33b889b26288693e51
Daniel Kroon <drg.kroon@gmail.com>
2017-05-14 22:44:29 +0200

    added toggleAll method

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,16 @@
 var todoList = {
 	todos: [],
 	displayTodos:	function() {
-		for (var i = 0; i < this.todos.length; i++) {
-			console.log(this.todos[i].todoText + " " + this.todos[i].time);
+		if (this.todos.length === 0) {
+			console.log('Your todo list is empty');
+		} else {
+			for (var i = 0; i < this.todos.length; i++) {
+				if (this.todos[i].completed === true) {
+					console.log("(X)", this.todos[i].todoText, this.todos[i].time);
+				} else {
+					console.log("( )", this.todos[i].todoText, this.todos[i].time);
+				}
+			}
 		}
 	},
 	addTodo: function(todoText, time) {
@@ -28,6 +36,32 @@ var todoList = {
 	toggleCompleted: function(position) {
 		var todo = this.todos[position];
 		todo.completed = !todo.completed;
+		this.displayTodos();
+	},
+	toggleAll: function() {
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+		
+		// Get number of compeleted todos
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++;
+			}
+		}
+		
+		// Case 1: If everything's true, make everything false.
+		if (completedTodos === totalTodos) {
+			// Make everything false
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+		// Case 2: Otherwise, make everything true.	
+		} else {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
+		
 		this.displayTodos();
 	}
 };
